7693c899c245875d8608846e41c807d3e21307dd
avainfo <avainfoconseils@gmail.com>
2025-10-02 14:48:52 +0200

    Removed useless imports

--- app/src/main/java/fr/avainfo/lessonm2ilyon/MainActivity.kt
+++ app/src/main/java/fr/avainfo/lessonm2ilyon/MainActivity.kt
@@ -4,21 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.unit.dp
 import fr.avainfo.lessonm2ilyon.ui.theme.LessonM2ILyonTheme
 
