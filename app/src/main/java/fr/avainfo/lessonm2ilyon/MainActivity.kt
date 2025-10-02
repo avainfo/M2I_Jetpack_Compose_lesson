6eed4f735562cfd84bbe795d93c4d109edf85dca
avainfo <avainfoconseils@gmail.com>
2025-10-02 14:40:31 +0200

    Clean Main base

--- app/src/main/java/fr/avainfo/lessonm2ilyon/MainActivity.kt
+++ app/src/main/java/fr/avainfo/lessonm2ilyon/MainActivity.kt
@@ -7,17 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import fr.avainfo.lessonm2ilyon.ui.theme.LessonM2ILyonTheme
-import fr.avainfo.lessonm2ilyon.widgets.Card
-import fr.avainfo.lessonm2ilyon.widgets.CustomIconButton
-import fr.avainfo.lessonm2ilyon.widgets.TopBar
-import fr.avainfo.lessonm2ilyon.widgets.styles.Title
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,34 +18,11 @@ class MainActivity : ComponentActivity() {
 		setContent {
 			LessonM2ILyonTheme {
 				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-					Column(
-						modifier = Modifier
-							.padding(innerPadding)
-							.padding(all = 24.dp)
-					) {
-						TopBar()
-						Title(
-							"Checkout",
-							Modifier
-								.padding(top = 24.dp)
-						)
-						Card(
-							modifier = Modifier
-								.size(250.dp)
-								.padding(top = 12.dp)
-						) {
-						}
+					Column(modifier = Modifier.padding(innerPadding)) {
+						// TODO : Modify Code here
 					}
 				}
 			}
 		}
-	}
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-	LessonM2ILyonTheme {
-		CustomIconButton()
 	}
 }
