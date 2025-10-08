5f9455118e8e9ef99763dd085ce0c6ca91080b3b
Antonin Do Souto <avainfoconseils@gmail.com>
2025-10-08 11:01:31 +0200

    Removed useless imports

--- app/src/main/java/fr/avainfo/lessonm2ilyon/screens/FirstScreen.kt
+++ app/src/main/java/fr/avainfo/lessonm2ilyon/screens/FirstScreen.kt
@@ -1,14 +1,12 @@
 package fr.avainfo.lessonm2ilyon.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
