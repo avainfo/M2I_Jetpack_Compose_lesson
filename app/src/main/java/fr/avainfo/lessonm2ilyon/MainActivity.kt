09995d3494718987741f69b857d97501ee7072cd
Antonin Do Souto <avainfoconseils@gmail.com>
2025-10-08 09:48:27 +0200

    Added navigation buttons and screens

--- app/src/main/java/fr/avainfo/lessonm2ilyon/MainActivity.kt
+++ app/src/main/java/fr/avainfo/lessonm2ilyon/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -16,41 +18,51 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RadialGradientShader
 import androidx.compose.ui.graphics.Shader
 import androidx.compose.ui.graphics.ShaderBrush
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.unit.dp
 import fr.avainfo.lessonm2ilyon.ui.theme.LessonM2ILyonTheme
 import fr.avainfo.lessonm2ilyon.widgets.CustomCard
 
 val cardGradient = object : ShaderBrush() {
-	override fun createShader(size: Size): Shader {
-		val biggerDimension = maxOf(size.width, size.height)
-		return RadialGradientShader(
-			colors = listOf(Color(0x40FFFFFF), Color(0x70000000)),
-			center = Offset(size.width / 2F, 0.05F),
-			radius = biggerDimension / 2f
-		)
-	}
+    override fun createShader(size: Size): Shader {
+        val biggerDimension = maxOf(size.width, size.height)
+        return RadialGradientShader(
+            colors = listOf(Color(0x40FFFFFF), Color(0x70000000)),
+            center = Offset(size.width / 2F, 0.05F),
+            radius = biggerDimension / 2f
+        )
+    }
 }
 
 class MainActivity : ComponentActivity() {
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		enableEdgeToEdge()
-		setContent {
-			LessonM2ILyonTheme {
-				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-					Column(modifier = Modifier.padding(innerPadding)) {
-						// TODO : Modify Code here
-						CustomCard(height = 75, width = 150, color = Color.Red) {
-							CustomCard(height = 70, width = 145, color = Color.Blue) {
-								CustomCard(height = 65, width = 140, color = Color.White) {
-									CustomCard(height = 60, width = 135, color = Color.Green) {
-										Text("Coucou")
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            val configuration = LocalConfiguration.current
+
+            val screenWidth = configuration.screenWidthDp
+            LessonM2ILyonTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Row(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize(),
+                        horizontalArrangement = Arrangement.SpaceAround
+                    ) {
+                        // Todo : Search shortcut to modify multiple ctrl + w
+                        CustomCard(height = 75, width = screenWidth / 4, color = Color.Blue) {
+                            Text("Page 1")
+                        }
+                        CustomCard(height = 75, width = screenWidth / 4, color = Color.White) {
+                            Text("Page 2")
+                        }
+                        CustomCard(height = 75, width = screenWidth / 4, color = Color.Red) {
+                            Text("Page 3")
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
